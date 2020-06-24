81fd7e66257e6083148c54b4f08ca58761bfd958
Dyram Val Alec Meyn <dyram.meyn@codingmart.com>
2020-06-24 11:20:03 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import LoginPage from "./components/LoginPage"
+import HomePage from "./components/HomePage"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          {/* <Route path="/" exact component={() => <Welcome />} /> */}
+          {/* <Route path="/signup" component={() => <Signup />} /> */}
+          <Route path="/login" component={() => <LoginPage />} />
+          <Route
+            crossorigin
+            path="/timeline"
+            component={() => <HomePage />}
+          />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
